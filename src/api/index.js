0de68bdd6078991d28780c20daddbd5e0826fb93
triwhiter <245886461@qq.com>
2020-07-31 15:56:23 +0800

    添加api接口

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+/*
+包含n个接口请求函数的模块
+函数的返回值: promise对象
+ */
+import ajax from './ajax';
+const BASE_URL = 'http://localhost:8088/bookstore';
+// const BASE_URL = '/bookstore'
+
+// 1、根据经纬度获取位置详情
+// export const reqAddress = (geohash) => ajax(`${BASE_URL}/position/${geohash}`);
+// 2、获取商品分类列表
+export const reqGoodCategorys = () => ajax(BASE_URL + '/bookclassify/all');

b95b13041d9c830a0a621f2a2e110fd1aead1f4a
iBoy <547142436@qq.com>
2020-08-01 12:46:02 +0800

    修改后台接口URL，初始化

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@
 函数的返回值: promise对象
  */
 import ajax from './ajax';
-const BASE_URL = 'http://localhost:8088/bookstore';
+const BASE_URL = 'http://localhost:8088/api';
 // const BASE_URL = '/bookstore'
 
 // 1、根据经纬度获取位置详情

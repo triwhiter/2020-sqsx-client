2e24766d8676e786f6994e29529128e48802def4
qmxwby <2621955028@qq.com>
2020-08-03 22:19:46 +0800

    增加分类搜索

--- src/components/GoodsList.vue
+++ src/components/GoodsList.vue
@@ -122,12 +122,30 @@ export default {
       this.icon[index] = "arrow-up-a";
       this.SET_GOODS_ORDER_BY(data);
     },
-    //获取后台的商品列表
+    //获取后台的商品列表,没有搜索
     loadGoodsList (){
       console.log(this.cid);
       console.log(this.searchItem);
       this.$http
       .get('/products/'+ this.current + '/' + this.pageSize + '/' + this.cid)
+      .then(resp => {
+        console.log(resp);
+         let res = resp.data;
+         if(res.code == 200){
+           console.log(res);
+           this.orderGoodsList = res.data.list;
+           this.total = res.data.total;
+         } else {
+           this.$Message.error(res.msg);
+         }
+      });
+    },
+    //获取后台商品列表，分类搜索
+    loadGoodsListSreach () {
+      console.log(this.cid);
+      console.log(this.searchItem);
+      this.$http
+      .get('/products/'+ this.searchItem + '/' + this.current + '/' + this.pageSize + '/' + this.cid)
       .then(resp => {
         console.log(resp);
          let res = resp.data;
@@ -159,8 +177,14 @@ export default {
       }
       if(this.$route.query.sreachData == null){
         this.cid = this.$route.query.cid;
+        this.searchItem = "";
       }
-      this.loadGoodsList();
+      if(this.searchItem == ""){
+        this.loadGoodsList();
+      } else {
+        this.loadGoodsListSreach();
+      }
+
     }
   },
   mounted() {
